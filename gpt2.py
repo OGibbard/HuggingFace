ce7d6161fe4447cd90a9640fa8b2fcdec6484ba9
OGibbard <gibbard.o@oundleschool.org.uk>
2023-06-18 18:51:50 +0100

    sentiment analysis on gpt2 results

--- gpt2.py
+++ gpt2.py
@@ -1,4 +1,8 @@
 from transformers import pipeline, set_seed
 generator = pipeline('text-generation', model='gpt2')
+classifier = pipeline("sentiment-analysis")
 set_seed(42)
-print(generator("Hello, I'm a language model,", max_length=30, num_return_sequences=5))
+numOfResults=5
+results=generator("Hello, I'm a language model,", max_length=20, num_return_sequences=numOfResults)
+for k in range(numOfResults):
+    print(str(results[k]['generated_text'])+ str(classifier(results[k]['generated_text'])))
