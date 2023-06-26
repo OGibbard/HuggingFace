42e2681cd780da23dc9b4d7ead2845bfa976ffcd
OGibbard <gibbard.o@oundleschool.org.uk>
2023-06-26 15:00:17 +0100

    Ask user for input image generation

--- imageGeneration.py
+++ imageGeneration.py
@@ -5,7 +5,7 @@ model_id = "runwayml/stable-diffusion-v1-5"
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None, requires_safety_checker = False)
 pipe = pipe.to("cuda")
 
-prompt = "Astronaut rides horse"
+prompt = input("What is your prompt?")
 image = pipe(prompt).images[0]
     
-image.save("results/astronaut_rides_horse.png")
+image.save("result.png")
